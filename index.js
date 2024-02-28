d9ef13389f7bd4d6aa96eb727d4ec3fc63190e85
Melvin Marcello <melvinmarcello2978@gmail.com>
2024-02-28 15:01:07 +0700

    adding coin data feature

--- index.js
+++ index.js
@@ -7,20 +7,63 @@ const options = {
 
 const buriwBot = new Bot(process.env.token, options)
 
-const prefix = "\\"
+const prefix = "/"
 
 const query = {
-    start: /^start$/,
+    start: new RegExp(`^/start$`),
+    help: new RegExp(`^/help$`),
+    price: new RegExp(`^${prefix}price`),
 }
 
 
-
-
-buriwBot.on("message", (res)=>{
-    buriwBot.sendMessage(res.from.id, "Halo Riw")
+buriwBot.onText(query.start, (res) =>{
+    const message = 'Welcome to Buriw Bot! \n' +  
+                   'This bot can help you with the following commands:\n\n'+  
+                   '/price => Get current top 10 coins prices.\n' +
+                   '/price {coins} => Get spesific coin data \n' +
+                   'ex: //price bitcoin';
+    buriwBot.sendMessage(res.from.id, message)
 })
 
-// buriwBot.onText(query.start, (res)=>{
-//     res.sendMessage(res.from.id, "Halo Riw")
+buriwBot.onText(query.help, (res) =>{
+    const message = 'Welcome to Buriw Bot! \n' +  
+                   'This bot can help you with the following commands:\n\n'+  
+                   '/price => Get current top 10 coins prices.\n' +
+                   '/price {coins} => Get spesific coin data \n' +
+                   'ex: //price bitcoin';
+    buriwBot.sendMessage(res.from.id, message)
+})
 
-// })
+
+buriwBot.onText(query.price, async (res) =>{
+    let resultText = []
+    let coins = res.text.split(' ')
+    if(coins.length == 1){
+        let data = await fetch(`${process.env.cg_base_url}/coins/markets?vs_currency=usd&per_page=10`)
+        let response = await data.json()
+        response.map(data =>{
+           resultText.push(`${data.name} => $${data.current_price}`)
+        })                        
+        
+        buriwBot.sendMessage(res.from.id, resultText.join('\n'))
+    }else{
+        let targetCoin = coins[1].toLowerCase();
+        let data = await fetch(`${process.env.cg_base_url}/coins/${targetCoin}?localization=false&tickers=true`)
+        let response = await data.json()            
+        
+        if(response.length){
+            const symbol = response.symbol.toUpperCase()
+            let message = `
+                ${symbol}\n$${response.market_data.current_price.usd}\nMarket Cap => 
+            `
+            
+            
+            buriwBot.sendPhoto(res.from.id, response.image.large, {
+                caption: message
+            })
+        }else{
+            buriwBot.sendMessage(res.from.id, "Sorry There is No Coin Data With That Name!")
+        }
+    }
+    
+})
